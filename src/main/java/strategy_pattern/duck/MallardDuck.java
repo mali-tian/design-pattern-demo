121671b6ae21f1249b86a76c3dd37dfdc73be525
mali.tian <mali.tian@rea-group.com>
2019-12-17 21:33:03 +0800

    restructure code

--- src/main/java/strategy_pattern/duck/MallardDuck.java
+++ src/main/java/strategy_pattern/duck/MallardDuck.java
@@ -1,16 +1,17 @@
-package hello.stragegy_pattern.duck;
+package strategy_pattern.duck;
 
 import strategy_pattern.behavior.FlyWithWings;
 import strategy_pattern.behavior.Quack;
 
-public class RedHeadDuck extends Duck {
-    public RedHeadDuck() {
+public class MallardDuck extends Duck {
+
+    public MallardDuck() {
         flyBehavior = new FlyWithWings();
         quackBehavior = new Quack();
     }
 
     @Override
     public void display() {
-        System.out.println("I am red head duck");
+        System.out.println("I am really a Mallard Duck");
     }
 }
